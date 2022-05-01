73c5633a74d072cc58e8d503eae24d736f815dea
LEANDRA PINHEIRO DA SILVA <202002553741@alunos.estacio.br>
2022-05-01 19:20:20 -0300

    adiciona components Task

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet, Text, SafeAreaView, Image, Button, Dimensions, View
-} from 'react-native';
-
-import header from './assets/header.png';
-
+import { StyleSheet, Text, View} from 'react-native';
+import Task from  './components/Task';
 
 export default function App() {
   return (
-    <SafeAreaView>
-  
-   </SafeAreaView>
+    <View styles={styles.container}>
+
+      {/*Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+        </View>
+
+      </View>
       
+      </View>
   );
 }
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+   
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+   sectionTitle: {},
+  items: {
+    marginTop:30,
+  },
+
+})
 
 
