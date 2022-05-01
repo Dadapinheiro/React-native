864c08f5d9e7e44fe4cafb3ae28ac813e2900c39
LEANDRA PINHEIRO DA SILVA <202002553741@alunos.estacio.br>
2022-05-01 13:00:45 -0300

    adiciona início do projeto

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet, Text, SafeAreaView, Image, Button, Dimensions, View
+} from 'react-native';
+
+import header from './assets/header.png';
+
+
+export default function App() {
+  return (
+    <SafeAreaView>
+  
+   </SafeAreaView>
+      
+  );
+}
+
+
